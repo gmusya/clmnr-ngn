7be9111dccdce591d1d4d19c7f1ba75c2eb1eaa4
German Perov <ggmusya@gmail.com>
2026-01-05 17:29:26 +0300

    Fix clang-tidy system headers

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -2,6 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(
   googletest
+  SYSTEM
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG        v1.17.0
 )
@@ -10,6 +11,7 @@ FetchContent_MakeAvailable(googletest)
 
 FetchContent_Declare(
   absl
+  SYSTEM
   GIT_REPOSITORY https://github.com/abseil/abseil-cpp.git
   GIT_TAG        lts_2025_08_14
 )
