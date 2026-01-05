83412837f408010a4ab8adb905b4dd586aeab993
German Perov <ggmusya@gmail.com>
2026-01-05 18:58:59 +0300

    Improve coverage

--- src/core/ut/csv_test.cpp
+++ src/core/ut/csv_test.cpp
@@ -40,4 +40,34 @@ TEST(CsvReader, Simple) {
   EXPECT_EQ(rows[5], (CsvReader::Row{"", "", "z"}));
 }
 
+TEST(CsvWriter, Simple) {
+  std::mt19937 rnd(2101);
+
+  std::filesystem::path path = std::filesystem::temp_directory_path() / std::to_string(rnd() % 10000);
+
+  CsvWriter writer(path);
+  writer.WriteRow({"a", "b", "c"});
+  writer.WriteRow({"q", "w", "e"});
+  writer.WriteRow({"d", "", "f"});
+  writer.WriteRow({"", "x", ""});
+  writer.WriteRow({"", "", ""});
+  writer.WriteRow({"", "", "z"});
+
+  std::ifstream file(path);
+  
+  std::vector<std::string> lines;
+  std::string line;
+  while (std::getline(file, line)) {
+    lines.emplace_back(line);
+  }
+
+  ASSERT_EQ(lines.size(), 6);
+  EXPECT_EQ(lines[0], "a,b,c");
+  EXPECT_EQ(lines[1], "q,w,e");
+  EXPECT_EQ(lines[2], "d,,f");
+  EXPECT_EQ(lines[3], ",x,");
+  EXPECT_EQ(lines[4], ",,");
+  EXPECT_EQ(lines[5], ",,z");
+}
+
 }  // namespace ngn
